0448a9cb411cce536700d2f0b0e11ec114d34b29
afernandezi22 <afernandezi22@fpsanjorge.com>
2023-11-10 14:25:27 +0100

    Ekipamenduan aldaketak ondo funtzionatzeko. BUKATU BARIK!!

--- html/ekipamendua.php
+++ html/ekipamendua.php
@@ -10,7 +10,6 @@
     <link rel="shortcut icon" href="../img/favicon2.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel='stylesheet' href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.min.js"> -->
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
     <title>Ekipamendua</title>
@@ -43,45 +42,100 @@
         <div class="content">
             <h1>EKIPAMENDUA</h1>
             <div class="botoiak">
-                <form action="">
-                    <button type="submit"><i class="fa-solid fa-circle-plus"></i></button>
-                    <button type="submit"><i class="fa-solid fa-pencil"></i></button>
-                    <button type="submit"><i class="fa-solid fa-trash"></i></button>
-                    <select name="bilaketa" id="bilaketa">
-                        <option value="id">ID zenbakia</option>
-                        <option value="izena">Ekipoaren izena</option>
-                        <option value="marka">marka</option>
-                        <option value="modelo">Modeloa</option>
-                        <option value="stock">Stock</option>
-                        <option value="idKategoria">Kategoria ID</option>
-                    </select>
-                    <input type="text" placeholder="Bilatu...">
-                    <button class="lupa" type="submit"><i class="fa fa-search"></i></button>
-                </form>
-            </div>
+                    <form action="">
+                        <button type="button" id="gehituButton"><i class="fa-solid fa-circle-plus"></i></button>
+                        <button type="button" id="editatuButton" disabled><i class="fa-solid fa-pencil"></i></button>
+                        <button type="button" id="ezabatuButton" disabled><i class="fa-solid fa-trash"></i></button>
+                        <select name="filtro" id="filtro">
+                            <option value="id">ID</option>
+                            <option value="izena">Ekipamenduaren izena</option>
+                            <option value="marka">Marka</option>
+                            <option value="modelo">Modeloa</option>
+                            <option value="stock">Stock</option>
+                            <option value="kategoria">Kategoria</option>
+                        </select>
+                        <input type="text" id="bilaketa" placeholder="Bilatu...">
+                        <select name="kategoria" id="kategoria" hidden></select>
+                        <button class="lupa" id="bilaketaButton" type="button"><i class="fa fa-search"></i></button>
+                        <button id="resetButton"><i class="fa-solid fa-rotate-right"></i></button>
+                    </form>
+                </div>
 
             <table id="ekipamenduaTable">
                 <tr>
-                <th></th>
-                <th class="sortable" data-column="id">ID<i class='bx bxs-sort-alt'></i></th>
-                <th class="sortable" data-column="izena">Izena<i class='bx bxs-sort-alt'></i></th>
-                <th>Deskribapena</th>
-                <th class="sortable" data-column="marka">Marka<i class='bx bxs-sort-alt'></i></th>
-                <th class="sortable" data-column="modelo">Modelo<i class='bx bxs-sort-alt'></i></th>
-                <th class="sortable" data-column="stock">Stock<i class='bx bxs-sort-alt'></i></th>
-            </tr>
+                    <th></th>
+                    <th>ID</th>
+                    <th>Izena</th>
+                    <th>Deskribapena</th>
+                    <th>Marka</th>
+                    <th>Modelo</th>
+                    <th>Stock</th>
+                    <th>Kategoria</th>
+                </tr>
 
                 <tbody id="showDataEkipamendua"></tbody>
             </table>
 
+            <div class="popup-container" id="gehituContainer">
+                <div class="popup">
+                    <h2>Gehitu</h2>
+                    <form id="gehituForm">
+                        <label for="gehituIzena">Izena:</label>
+                        <input type="text" id="gehituIzena" name="gehituIzena" required>
+                        <br><br>
+                        <label for="gehituDeskribapena">Deskribapena:</label>
+                        <input type="text" id="gehituDeskribapena" name="gehituDeskribapena" required>
+                        <br><br>
+                        <label for="gehituMarka">Marka:</label>
+                        <input type="text" id="gehituMarka" name="gehituMarka" required>
+                        <br><br>
+                        <label for="gehituModelo">Modelo:</label>
+                        <input type="text" id="gehituModelo" name="gehituModelo" required>
+                        <br><br>
+                        <label for="gehituKategoria">Kategoria:</label>
+                        <select name="gehituKategoria" id="gehituKategoria"></select>
+                        <br><br>
+                        <button type="submit" id="gehituSubmit">Onartu</button>
+                        <button type="button" id="itxiGehituPopup">Itxi</button>
+                    </form>
+                </div>
+            </div>
+
+            <div class="popup-container" id="editatuContainer">
+                <div class="popup">
+                    <h2>Editatu</h2>
+                    <form id="editatuForm">
+                        <label for="editatuId">ID:</label>
+                        <input type="number" id="editatuId" name="editatuId" disabled>
+                        <br><br>
+                        <label for="editatuIzena">Izena:</label>
+                        <input type="text" id="editatuIzena" name="editatuIzena" required>
+                        <br><br>
+                        <label for="editatuDeskribapena">Deskribapena:</label>
+                        <input type="text" id="editatuDeskribapena" name="editatuDeskribapena" required>
+                        <br><br>
+                        <label for="editatuMarka">Marka:</label>
+                        <input type="text" id="editatuMarka" name="editatuMarka" required>
+                        <br><br>
+                        <label for="editatuModelo">Modelo:</label>
+                        <input type="text" id="editatuModelo" name="editatuModelo" required>
+                        <br><br>
+                        <label for="editatuKategoria">Kategoria:</label>
+                        <select name="editatuKategoria" id="editatuKategoria"></select>
+                        <br><br>
+                        <button type="submit" id="editatuSubmit">Onartu</button>
+                        <button type="button" id="itxiEditatuPopup">Itxi</button>
+                    </form>
+                </div>
+            </div>
+
             <div class="tab-control">
-                <img class="geziak" src="../img/flecha-izquierda.png" id="previous" onclick="paginar(-1, 'ekipamenduaTable')" />
+                <img class="geziak" src="../img/flecha-izquierda.png" id="previous" onclick="paginarEkipamendua(-1)" />
                 <span id="page-number">1</span> / <span id="total-pages">-</span>
-                <img class="geziak" src="../img/flecha-derecha.png" id="next" onclick="paginar(1, 'ekipamenduaTable')" />
+                <img class="geziak" src="../img/flecha-derecha.png" id="next" onclick="paginarEkipamendua(1)" />
             </div>
         </div>
 
-        <script src="../js/viewTables.js"></script>
         <script src="../js/ekipamendua.js"></script>
 
         <!-- FOOTER -->
--- js/ekipamendua.js
+++ js/ekipamendua.js
@@ -1,73 +1,349 @@
-// DATUAK TAULAN BISTARAZTEKO PAGINA KARGATZERAKOAN
-window.addEventListener("load", function(){
-    if(document.getElementById("ekipamenduaTable")!= undefined){
-        getDataFromURL("ekipamenduaTable");
-        const th = document.getElementsByTagName("th");
-        for (let i = 0; i < th.length; i++) {
-            th[i].addEventListener("click", headerClicked);
+//BOTOIAK
+const ezabatuButton = document.getElementById("ezabatuButton");
+const gehituButton = document.getElementById("gehituButton");
+const editatuButton = document.getElementById("editatuButton");
+const bilaketaButton = document.getElementById("bilaketaButton");
+const resetButton = document.getElementById("resetButton");
+const bilaketaTestu = document.getElementById("bilaketa");
+const filtroSelect = document.getElementById("filtro");
+
+// BOTOIAK AKTIBATU ETA DESAKTIBATZEKO
+document.addEventListener("DOMContentLoaded", function () {
+    const checkboxContainer = document.getElementById("ekipamenduaTable");
+    const editatuButton = document.getElementById("editatuButton");
+    const ezabatuButton = document.getElementById("ezabatuButton");
+
+    checkboxContainer.addEventListener("change", function (event) {
+        if (event.target.classList.contains("checkbox-item")) {
+            const checkedCheckboxes = Array.from(checkboxContainer.querySelectorAll('.checkbox-item:checked'));
+            if(checkedCheckboxes.length === 0) {
+                ezabatuButton.disabled = true;
+                editatuButton.disabled = true;
+            } else if (checkedCheckboxes.length === 1) {
+                editatuButton.disabled = false;
+                ezabatuButton.disabled = false;
+            } else {
+                editatuButton.disabled = true;
+            }
         }
-    }
+    });
 });
 
+//GEHITZEKO LOGIKA
+const gehituContainer = document.getElementById("gehituContainer");
+const itxiGehituPopup = document.getElementById("itxiGehituPopup");
+const gehituForm = document.getElementById("gehituForm");
+const gehituKategoriaSelect = document.getElementById("gehituKategoria");
+const editatuKategoriaSelect = document.getElementById("editatuKategoria");
+const kategoriaSelect = document.getElementById("kategoria");
+
+function kategoriakKargatu() {
+    // Vacía los elementos del desplegable
+    gehituKategoriaSelect.innerHTML = '';
+    editatuKategoriaSelect.innerHTML = '';
+
+    fetch('http://localhost/erronka1/controller/kategoriacontroller.php')
+        .then(response => response.json())
+        .then(data => {
+            // Itera sobre el JSON y agrega opciones al desplegable
+            data.forEach(item => {
+                var optionGehi = document.createElement('option');
+                optionGehi.value = item.id;
+                optionGehi.text = item.izena;
+                var optionEdit = document.createElement('option');
+                optionEdit.value = item.id;
+                optionEdit.text = item.izena;
+                var optionFilter = document.createElement('option');
+                optionFilter.value = item.id;
+                optionFilter.text = item.izena;
+                gehituKategoriaSelect.appendChild(optionGehi);
+                editatuKategoriaSelect.appendChild(optionEdit);
+                kategoriaSelect.appendChild(optionFilter);
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    
+}
+
+gehituButton.addEventListener("click", function () {
+    gehituContainer.style.display = "block";
+});
+
+itxiGehituPopup.addEventListener("click", function () {
+    gehituContainer.style.display = "none";
+});
+
+gehituForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    insertData();
+    gehituContainer.style.display = "none";
+});
+
+function insertData(){
+    var gehituIzenaValue = document.getElementById("gehituIzena").value;
+    var gehituDeskribapenaValue = document.getElementById("gehituDeskribapena").value;
+    var gehituMarkaValue = document.getElementById("gehituMarka").value;
+    var gehituModeloValue = document.getElementById("gehituModelo").value;
+    var gehituKategoriaValue = document.getElementById("gehituKategoria").value;
+
+    const data = {
+        izena: gehituIzenaValue,
+        deskribapena: gehituDeskribapenaValue,
+        marka: gehituMarkaValue,
+        modelo: gehituModeloValue,
+        idKategoria: gehituKategoriaValue
+    };
+    fetch('http://localhost/erronka1/controller/ekipamenduacontroller.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+    .then(() => {
+        console.log("ONDO!");
+        getData();
+    })
+    .catch(error => {
+        console.log("ERROR! " + error);
+    });
+
+}
+
+//EDITATZEKO LOGIKA
+const editatuContainer = document.getElementById("editatuContainer");
+const itxiEditatuPopup = document.getElementById("itxiEditatuPopup");
+const editatuForm = document.getElementById("editatuForm");
+
+editatuButton.addEventListener("click", function () {
+    const checkbox = document.querySelector('.checkbox-item:checked');
+    const editatuIdInput = document.getElementById("editatuId");
+    editatuIdInput.value = checkbox.id;
+
+    editatuContainer.style.display = "block";
+});
+
+itxiEditatuPopup.addEventListener("click", function () {
+    editatuContainer.style.display = "none";
+});
+
+editatuForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    editData();
+    editatuContainer.style.display = "none";
+});
+
+function editData(){
+    const checkbox = document.querySelector('.checkbox-item:checked');
+    const editatuIdInputValue = document.getElementById("editatuId").value;
+    const editatuIzenaInputValue = document.getElementById("editatuIzena").value;
+    const editatuDeskribapenaInputValue = document.getElementById("editatuDeskribapena").value;
+    const editatuMarkaInputValue = document.getElementById("editatuMarka").value;
+    const editatuModeloInputValue = document.getElementById("editatuModelo").value;
+    const editatuKategoriaInputValue = document.getElementById("editatuKategoria").value;
+
+    const data = {
+        id: editatuIdInputValue,
+        izena: editatuIzenaInputValue,
+        deskribapena: editatuDeskribapenaInputValue,
+        marka: editatuMarkaInputValue,
+        modelo: editatuModeloInputValue,
+        idKategoria: editatuKategoriaInputValue
+    }
+
+    
+    fetch('http://localhost/erronka1/controller/ekipamenduacontroller.php', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+    .then(() => {
+        console.log("ONDO!");
+        getData();
+    })
+    .catch(error => {
+        console.log("ERROR! " + error);
+    });
+
+    ezabatuButton.disabled = true;
+    editatuButton.disabled = true;
+}
+
+//EZABATZEKO LOGIKA
+function deleteData(){
+    // Lortu ID-ak
+    const checkboxContainer = document.getElementById("ekipamenduaTable");
+    const checkedCheckboxes = Array.from(checkboxContainer.querySelectorAll('.checkbox-item:checked'));
+
+    const checkboxIDs = [];
+
+    checkedCheckboxes.forEach(checkbox => {
+        checkboxIDs.push(checkbox.id);
+    });
+
+    console.log(checkboxIDs);
+
+    const data = {
+        id: checkboxIDs
+    }
+    
+    //Galdetu
+    const konfirmatu = window.confirm("Ziur al zaude " + checkboxIDs + " elementuak ezabatu nahi dituzula?");
+    if(konfirmatu){
+        //Deia egin
+        fetch('http://localhost/erronka1/controller/ekipamenduacontroller.php', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+        },
+            body: JSON.stringify(data),
+        })
+        .then(() => {
+            console.log("ONDO EZABATUTA!");
+            getData();
+        })
+        .catch(error => {
+            console.log("ERROR! " + error);
+        });
+    }
+    
+    ezabatuButton.disabled = true;
+    editatuButton.disabled = true;
+}
+
+ezabatuButton.addEventListener("click", function (){
+    deleteData();
+});
+
+//PAGINATZEKO LOGIKA
+var dataEkipamendua = [];
+const tableLines = 10;
+var actualPag = 1;
+
+//PAGINAR LA TABLA EKIPAMENDUA
+function paginarEkipamendua(direccion) {
+    let totalPages = Math.ceil(dataEkipamendua.length / tableLines);
+
+    actualPag += direccion;
+    if (actualPag < 1) {
+        actualPag = 1;
+    }
+    if (actualPag > totalPages) {
+        actualPag = totalPages;
+    }
+
+    viewTableEkipamendua(dataEkipamendua, actualPag);
+
+    document.getElementById("page-number").innerHTML = actualPag;
+    document.getElementById("total-pages").innerHTML = totalPages;
+}
+
+function getData() {
+    fetch('http://localhost/erronka1/controller/ekipamenduacontroller.php', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        dataEkipamendua = data;
+        totalPages = Math.ceil(dataEkipamendua.length / tableLines);
+        paginarEkipamendua(0); // Para asegurar que se inicie en la página 1
+        kategoriakKargatu();
+    })
+    .catch(err => {
+        console.error("ERROR: " + err.message);
+    })
+}
 
 
-// DATUAK TAULA FORMATUAN BISTARATZEKO
-function viewTable(dataAll, actualPag, tableId) {
+function viewTableEkipamendua(dataEkipamendua, actualPag) {
     var tableHtml = "";
     var start = (actualPag - 1) * tableLines;
     var end = start + tableLines;
-
-    for (let i = start; i < Math.min(end, tableData[tableId].data.length); i++) {
-        if (tableId == "ekipamenduaTable"){
-            tableHtml += "<tr><td><input type='checkbox' id=" + tableData[tableId].data[i]['id'] + "></td>";
-            tableHtml += "<td>" + tableData[tableId].data[i]["id"] + "</td>";
-            tableHtml += "<td>" + tableData[tableId].data[i]["izena"] + "</td>";
-            tableHtml += "<td>" + tableData[tableId].data[i]["deskribapena"] + "</td>";
-            tableHtml += "<td>" + tableData[tableId].data[i]["marka"] + "</td>";
-            tableHtml += "<td>" + tableData[tableId].data[i]["modelo"] + "</td>";
-            tableHtml += "<td>" + tableData[tableId].data[i]["stock"] + "</td></tr>";
-        }
-    }
     
-    //console.log("tableHtml" + tableHtml);
+    for (var i = start; i < Math.min(end, dataEkipamendua.length); i++){
+        tableHtml += "<tr><td><input type='checkbox' class='checkbox-item' id=" + dataEkipamendua[i]["id"] + "></td>";
+        tableHtml += "<td>" + dataEkipamendua[i]["id"] + "</td>";
+        tableHtml += "<td>" + dataEkipamendua[i]["izena"] + "</td>";
+        tableHtml += "<td>" + dataEkipamendua[i]["deskribapena"] + "</td>";
+        tableHtml += "<td>" + dataEkipamendua[i]["marka"] + "</td>";
+        tableHtml += "<td>" + dataEkipamendua[i]["modelo"] + "</td>";
+        tableHtml += "<td>" + dataEkipamendua[i]["stock"] + "</td>";
+        tableHtml += "<td>" + dataEkipamendua[i]["kategoriaIzena"] + "</td></th>";
+    }
     document.getElementById("showDataEkipamendua").innerHTML = tableHtml;
 }
-var asc = true;
-// DATUAK ZUBETATIK ORDENATZEKO
-//const th = document.getElementsByTagName("th");
-function headerClicked(e) {
-    // console.log(e.target.parentNode.cellIndex);
-    const sortColumn = e.target.cellIndex !== undefined ? e.target.cellIndex : e.target.parentNode.cellIndex;
-    //console.log(sortColumn);
-    asc = !asc;
-    sortTableByColumn(sortColumn);
+
+window.addEventListener("load", function(){
+    getData();
+})
+
+// FILTROA
+bilaketaButton.addEventListener("click", function(){
+    filterData();
+});
+
+bilaketaTestu.addEventListener("keypress", function(event) {
+    if(event.key === "Enter") {
+        event.preventDefault();
+        filterData();
+    }
+});
+
+function filterData(){
+    const bilaketaInputValue = document.getElementById("bilaketa").value;
+    const filtroSelectValue = document.getElementById("filtro").value;
+    // FALTA PONER AQUÍ LO DE KATEGORIA!!!!
+
+    const url = `http://localhost/erronka1/controller/ekipamenduacontroller.php?datua=${bilaketaInputValue}&zutabea=${filtroSelectValue}`;
+
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        if(data == null){
+            alert("Kontuz! Ez dago daturik kontsulta horrekin!");
+        } else {
+            dataEkipamendua = data;
+            totalPages = Math.ceil(dataEkipamendua.length / tableLines);
+            paginarEkipamendua(0); // Para asegurar que se inicie en la página 1
+        }
+    })  
+    .catch(err => {
+        console.error("ERROR: " + err.message);
+    })
 }
 
-function sortTableByColumn(sortColumn) {
-    const tableBody = document.getElementById("showDataEkipamendua");
-    // console.log(tableBody);
-    const rows = Array.from(tableBody.rows);
-    // console.log(rows);
-    //var asc = true;
-    if (asc === true){
-        var sortedRows = rows.sort(function(a,b){
-            // console.log(a.cells[sortColumn]);
-            const aText = a.cells[sortColumn].textContent;
-            const bText = b.cells[sortColumn].textContent;
-            return aText.localeCompare(bText);
-        });
+filtroSelect.addEventListener('change', function() {
+    // Obtén el valor seleccionado
+    var selectedOption = filtroSelect.value;
+
+    // Verifica si la opción seleccionada es "hasieraData" o "amaieraData"
+    if (selectedOption === 'kategoria') {
+        // Cambia el tipo de input a "date"
+        bilaketaTestu.hidden = true;
+        kategoriaSelect.hidden = false;
+    } else if(selectedOption === 'id') {
+        bilaketaTestu.hidden = false;
+        bilaketaTestu.type = 'number';
+    } else{
+        bilaketaTestu.hidden = false;
+        bilaketaTestu.type = 'text';
     }
-    if (asc === false){
-        var sortedRows = rows.sort(function(a,b){
-            // console.log(a.cells[sortColumn]);
-            const aText = a.cells[sortColumn].textContent;
-            const bText = b.cells[sortColumn].textContent;
-            return bText.localeCompare(aText);
-        });
-    }
-    //console.log(sortedRows);
-    tableBody.innerHTML = ""; 
-    sortedRows.forEach(row=>{
-        tableBody.appendChild(row);
-    });
-}
+});
+
+resetButton.addEventListener("click", function(){
+    getData();
+});
